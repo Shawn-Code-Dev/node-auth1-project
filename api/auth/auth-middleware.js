63397f879a26d0f10282fb32e43f75179af7affe
Shawn Rice <Shawn.CS@protonmail.com>
2021-10-11 22:13:43 -0400

    Complete

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,3 +1,5 @@
+const { findBy } = require("../users/users-model")
+
 /*
   If the user does not have a session saved in the server
 
@@ -6,8 +8,15 @@
     "message": "You shall not pass!"
   }
 */
-function restricted() {
-
+function restricted(req, res, next) {
+  if (req.session.user) {
+    next()
+  } else {
+    next({
+      message: 'You shall not pass!',
+      status: 401
+    })
+  }
 }
 
 /*
@@ -18,8 +27,21 @@ function restricted() {
     "message": "Username taken"
   }
 */
-function checkUsernameFree() {
-
+async function checkUsernameFree(req, res, next) {
+  try {
+    const { username } = req.body
+    const taken = await findBy({ username }).first()
+    if (taken) {
+      next({
+        status: 422,
+        message: `Username taken`
+      })
+    } else {
+      next()
+    }
+  } catch (err) {
+    next(err)
+  }
 }
 
 /*
@@ -30,8 +52,21 @@ function checkUsernameFree() {
     "message": "Invalid credentials"
   }
 */
-function checkUsernameExists() {
-
+async function checkUsernameExists(req, res, next) {
+  try {
+    const { username } = req.body
+    const exists = await findBy(username).first()
+    if (exists) {
+      next()
+    } else {
+      next({
+        status: 401,
+        message: `Invalid credentials`
+      })
+    }
+  } catch (err) {
+    next(err)
+  }
 }
 
 /*
@@ -42,8 +77,28 @@ function checkUsernameExists() {
     "message": "Password must be longer than 3 chars"
   }
 */
-function checkPasswordLength() {
-
+async function checkPasswordLength(req, res, next) {
+  try {
+    const { password } = req.body
+    if (!password || password.length <= 3){
+      next({
+        status: 422,
+        message: "Password must be longer than 3 chars"
+      })
+    }
+    next()
+  } catch (err) {
+    next({
+      status: 422,
+      message: err.message
+    })
+  }
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
+module.exports = {
+  restricted,
+  checkUsernameFree,
+  checkUsernameExists,
+  checkPasswordLength,
+}
--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -2,6 +2,13 @@
 // middleware functions from `auth-middleware.js`. You will need them here!
 
 
+const { checkUsernameFree, checkUsernameExists, checkPasswordLength } = require('./auth-middleware');
+const bcrypt = require('bcryptjs')
+
+const User = require('../users/users-model')
+
+const router = require('express').Router();
+
 /**
   1 [POST] /api/auth/register { "username": "sue", "password": "1234" }
 
@@ -25,7 +32,57 @@
   }
  */
 
+router.post('/register', checkUsernameFree, checkPasswordLength, async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const hash = bcrypt.hashSync(password, 8);
+    const user = { username, password: hash };
+    const result = await User.add(user)
+    const { user_id } = result
+    res.status(200).json({user_id, username})
+  } catch (err) {
+    next(err)
+  }
+})
 
+router.post('/login', async (req, res, next) => {
+  try {
+    const { username, password } = req.body
+    const user = await User.findBy({ username }).first()
+    if (user && bcrypt.compareSync(password, user.password)) {
+      req.session.user = user
+      res.status(200).json({ message: `Welcome ${user.username}!` })
+    } else {
+      next({
+        status: 401,
+        message: 'invalid credentials'
+      })
+    }
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/logout', async (req, res, next) => {
+  if (req.session.user) {
+    req.session.destroy((err) => {
+      if (err) {
+        res.json({
+          message: 'you cannot leave!'
+        })
+      } else {
+        // set a cookie in the past
+        res.json({
+          message: `logged out`
+        })
+      }
+    })
+  } else {
+    res.json({
+       message: 'no session'
+    })
+  }
+})
 /**
   2 [POST] /api/auth/login { "username": "sue", "password": "1234" }
 
@@ -61,3 +118,4 @@
 
  
 // Don't forget to add the router to the `exports` object so it can be required in other modules
+module.exports = router
